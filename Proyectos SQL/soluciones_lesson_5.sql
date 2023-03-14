efe18f4d9efc7e02887a6c3a8d53fb88256f2bce
Cristian Torres <cristiantorr98@gmail.com>
2023-03-14 16:15:56 +0100

    PROYECTOS SQL

---
+++ Proyectos SQL/soluciones_lesson_5.sql
@@ -0,0 +1,20 @@
+-- 1
+select * from sol_2_5;
+-- 2
+select * from sol_5_2;
+-- 3
+select * from sol_2_8;
+-- 4
+select * from sol_2_10;
+-- 5
+select * from sol_2_12;
+-- 6
+select * from sol_2_14;
+-- 7
+select * from sol_2_15;
+-- 8
+select * from sol_2_19;
+-- 9
+select * from sol_2_23;
+-- 10
+select * from sol_2_24;
---
+++ Proyectos SQL/soluciones_lesson_6.sql
@@ -0,0 +1,20 @@
+-- 1
+select * from sol_6_1;
+-- 2
+select * from docencia.sol_6_2;
+-- 3
+select * from sol_3_2;
+-- 4
+select * from sol_3_4;
+-- 5
+select * from sol_3_8;
+-- 6
+select * from sol_3_10;
+-- 7
+select * from sol_3_13;
+-- 8
+select * from sol_3_15;
+-- 9
+select * from sol_3_18;
+-- 10
+select * from sol_3_20;
